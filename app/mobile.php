769fe2d1fba7b04583604f7e1885f7fb1651881c
Luigi Coletti <luigi.coletti04@gmail.com>
2023-03-18 21:58:23 +0100

    added new pause functions and some design improvements

--- app/mobile.php
+++ app/mobile.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="../resources/css/app-mobile-1.css">
     <script src="../resources/js/autonumeric.js"></script>
     <script src="../resources/js/jquery-3.6.4.min.js"></script>
+    <script src="https://kit.fontawesome.com/4cc784d7e8.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div id="main">
@@ -14,40 +15,49 @@
             <h2 class="top-left">Hallo, Luigi!</h2>
         </div>
         <div id="center">
-            <!--
+            <div id="desc-center">
+            </div>
             <div id="timer">
                 <label id="hours">00</label>:<label id="minutes">00</label>:<label id="seconds">00</label>
             </div>
-            -->
-            <div id="form">
+            <div id="form-start">
                 <form name="meta-start" method="post">
                     <input class="inp-fw" name="loc-start" type="text" placeholder="Startort..." value=""/>
                     <input id="numeric" class="inp-fw" name="km-start" type="text" placeholder="KM Stand..." value=""/>
                 </form>
             </div>
+            <div id="pause-view">
+                <label for="#pause">Pausiert</label>
+                <i id="pause" class="fa-solid fa-circle-pause"></i>
+                <div id="form-pause">
+                    <form name="meta-pause" method="post">
+                        <input class="inp-fw" name="loc-pause" type="text" placeholder="Standort..." value=""/>
+                    </form>
+                </div>
+            </div>
         </div>
         <div id="bottom">
             <div id="buttons">
                 <div id="left">
-                    <button id="btn-start" onclick="checkInput();">Start</button>
-                    <button id="btn-resume">Weiter</button>
+                    <button id="btn-start" onclick="startFunc();">Start</button>
+                    <button id="btn-resume" onclick="resumeFunc();">Weiter</button>
                 </div>
                 <div id="right">
                     <button id="btn-manual">Manuell</button>
                     <button id="btn-stop">Beenden</button>
                 </div>
                 <div id="stretch">
-                    <button id="btn-pause">Pause</button>
+                    <button id="btn-pause" onclick="pauseFunc();">Pause</button>
                     <button id="btn-save">Speichern</button>
                 </div>
             </div>
         </div>
     </div>
 </body>
-<!--<script src="../resources/js/timer.js"></script>-->
+<script src="../resources/js/timer.js"></script>
 <script src="../resources/js/main-functions.js"></script>
 <script>
-    loadSaved();
+    loadView();
     $(function() {
         new AutoNumeric('#numeric', {currencySymbol :' km', allowDecimalPadding:'false',currencySymbolPlacement:'s',digitGroupSeparator:"'"});
     });
